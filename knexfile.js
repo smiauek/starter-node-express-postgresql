0b818ba74dd0aa583c3d53b74d944f6e33f382a3
Piotr Smialkowski <smiaucius@gmail.com>
2022-01-24 13:27:41 -0600

    created initial migration files

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const { DATABASE_URL } = process.env;
+const path = require("path");
 
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
@@ -8,5 +9,8 @@ module.exports = {
   development: {
     client: "postgresql",
     connection: DATABASE_URL,
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    }
   },
 };
